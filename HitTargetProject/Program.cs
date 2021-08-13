dc0463f8b6a4b0fce686fd78caea237262cebbb4
Enoch Mwangi <enoch.mwangi@outlook.com>
2021-08-13 21:41:57 +0300

    Incorporated methods to find the solution.

--- HitTargetProject/Program.cs
+++ HitTargetProject/Program.cs
@@ -4,13 +4,20 @@ namespace HitTargetProject
 {
     class Program
     {
+        static double Time(double a, double b, double c, double d)
+        {
+            return a / (b * Math.Cos(c * d));
+        }
+        static double Height(double a,double b, double c,double d,double e)
+        {
+            return ((0.5 * -1) * a * b * b) + (c * b * Math.Sin(d * e) + 10.75);
+        }
         static void Main(string[] args)
         {
-            const double PI = 3.14;
-            const double degrees = PI / 180;
+            const double degrees = Math.PI / 180;
             const double grav = 9.81;
             double len = 3.5;
-            int sum1 = 0;
+            int sum1 = 0; 
             int sum2 = 0;
 
             Console.Write("==PLAYER 1==\nVelocity:");
@@ -19,8 +26,8 @@ namespace HitTargetProject
             Console.Write("Angle:");
             var angle1 = Convert.ToDouble(Console.ReadLine());
 
-            var T1 = len / (vel1 * Math.Cos(angle1 * degrees));
-            var height1 = ((0.5 * -1) * grav * T1 * T1) + (vel1 * T1 * Math.Sin(angle1 * degrees) + 10.75);
+            var T1 = Time(len, vel1, angle1, degrees);
+            var height1 = Height(grav,T1,vel1,angle1,degrees);
 
             Console.Write("==PLAYER 2==\nVelocoty:");
             var vel2 = Convert.ToDouble(Console.ReadLine());
@@ -28,8 +35,8 @@ namespace HitTargetProject
             Console.Write("Angle:");
             var angle2 = Convert.ToDouble(Console.ReadLine());
 
-            var T2 = len / (vel2 * Math.Cos(angle2 * degrees));
-            var height2 = ((0.5 * -1) * grav * T2 * T2) + (vel2 * T2 * Math.Sin(angle2 * degrees) + 10.75);
+            var T2 = Time(len, vel2, angle2, degrees);
+            var height2 = Height(grav, T2, vel2, angle2, degrees);
 
             if (height1 < 3 || height1 > 3.5)
             {
